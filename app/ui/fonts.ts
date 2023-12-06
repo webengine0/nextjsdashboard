3f56d181a629c26df5accb294cb7aa1d25eac7b6
webengine0 <m.abdullah4757@gmail.com>
2023-12-06 20:25:54 +0500

    Next-js Dashoard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Roboto_Mono,Lusitana } from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+})
